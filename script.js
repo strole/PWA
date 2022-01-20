673dd959609029f87c4789a91bd54fc0a45b5eb6
Luka Štroliga <lstrol00@fesb.hr>
2022-01-20 19:45:23 +0100

    commit

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const rollingDices = document.getElementById("rollingDices");
 const rollingDice = document.getElementById("rollingDice");
 const dice2 = document.getElementById("dice2");
 const dice = document.getElementById("dice");
-const result = document.getElementById("result");
+const rezultat = document.getElementById("result");
 const button = document.querySelector("button");
 const konzola = document.getElementById("console");
 
@@ -21,14 +21,14 @@ document.getElementById("button").onclick = function () {
       dice.src = "/dices/dice" + randomNumber + ".svg";
       let randomNumber2 = Math.floor(Math.random() * 6 + 1);
       dice2.src = "/dices/dice" + randomNumber2 + ".svg";
-      result.innerHTML = "Your result is: " + (randomNumber + randomNumber2);
+      rezultat.innerHTML = "Your result is: " + (randomNumber + randomNumber2);
     } else {
       rollingDice.style.display = "none";
       dice2.style.display = "none";
       dice.style.display = "block";
       let randomNumber = Math.floor(Math.random() * 6 + 1);
       dice.src = "/dices/dice" + randomNumber + ".svg";
-      result.innerHTML = "Your result is: " + randomNumber;
+      rezultat.innerHTML = "Your result is: " + randomNumber;
     }
     button.disabled = false;
   }, 1000);
@@ -62,7 +62,7 @@ navigator.permissions.query({ name: "accelerometer" }).then((result) => {
   acl.addEventListener("error", (error) => (konzola = "Error: " + error.name));
   acl.addEventListener("reading", () => {
     let magnitude = Math.hypot(acl.x, acl.y, acl.z);
-    if (magnitude > 40) {
+    if (magnitude > 45) {
       setTimeout(function () {
         if (checkBox.checked == true) {
           rollingDices.style.display = "none";
@@ -72,7 +72,7 @@ navigator.permissions.query({ name: "accelerometer" }).then((result) => {
           dice.src = "/dices/dice" + randomNumber + ".svg";
           let randomNumber2 = Math.floor(Math.random() * 6 + 1);
           dice2.src = "/dices/dice" + randomNumber2 + ".svg";
-          konzola.innerHTML =
+          rezultat.innerHTML =
             "Your result is: " + (randomNumber + randomNumber2);
         } else {
           rollingDice.style.display = "none";
@@ -80,7 +80,7 @@ navigator.permissions.query({ name: "accelerometer" }).then((result) => {
           dice.style.display = "block";
           let randomNumber = Math.floor(Math.random() * 6 + 1);
           dice.src = "/dices/dice" + randomNumber + ".svg";
-          konzola.innerHTML = "Your result is: " + randomNumber;
+          rezultat.innerHTML = "Your result is: " + randomNumber;
         }
         button.disabled = false;
       }, 1000);
