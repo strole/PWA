3b48bee87879fb804dbca88ea37678329835609d
Luka Štroliga <lstrol00@fesb.hr>
2022-01-23 19:03:53 +0100

    commit

--- service-worker.js
+++ service-worker.js
@@ -20,13 +20,13 @@ self.addEventListener("install", function (event) {
 self.addEventListener("activate", (event) => {
   console.log("Activated, V1 now ready to handle fetches!");
 });
-self.addEventListener("fetch", (event) => {
-  const url = new URL(event.request.url);
-  // serve the cat SVG from the cache if the request is
-  // same-origin and the path is '/dog.svg'
-  if (url.origin == location.origin && url.pathname == "/dog.svg") {
-    event.respondWith(caches.match("/cat.svg"));
-  }
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
 
 self.addEventListener("sync", function (event) {
