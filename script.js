4b1ca998e4b2e5dc5edba07ae708a7287fda3db4
Luka Štroliga <lstrol00@fesb.hr>
2022-01-24 00:13:24 +0100

    commit

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const httpsLocalhost = require("https-localhost")();
+// const app = ...
+// const port = 443
+const certs = await httpsLocalhost.getCerts();
+const server = https.createServer(certs, app).listen(port);
+
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/service-worker.js");
 }
