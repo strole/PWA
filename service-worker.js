4b9ac7e4e9f4af41876e473c8bfca725b0fc09d4
Luka Štroliga <lstrol00@fesb.hr>
2022-01-25 18:41:04 +0100

    commit

--- service-worker.js
+++ service-worker.js
@@ -23,9 +23,18 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+    (async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) {
+        return r;
+      }
+      const response = await fetch(e.request);
+      const cache = await caches.open("static");
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })()
   );
 });
 
